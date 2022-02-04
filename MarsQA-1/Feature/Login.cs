e40372caa3243b9d258b31ce4749abfbe8f8dc95
Sreena Narikot <sreesat25@yahoo.co.in>
2022-02-04 11:23:17 +0800

    first commit

---
+++ MarsQA-1/Feature/Login.cs
@@ -0,0 +1,47 @@
+﻿using MarsQA_1.Helpers;
+using MarsQA_1.Pages;
+using MarsQA_1.SpecflowPages.Pages;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature
+{
+    [Binding]
+    class Login : Driver
+    {
+        [Given(@": I logged into the URL succsfully")]
+        public void GivenILoggedIntoTheURLuccsfully()
+        {
+
+            Driver driver = new Driver();
+            driver.Initialize();
+            Driver.TurnOnWait();
+            Driver.NavigateUrl();
+            SignIn.Login();
+
+        }
+
+        [Given(@": I navigate to profilepage")]
+        public void GivenINavigateToProfilepage()
+        {
+            HomePage homePage = new HomePage();
+        }
+
+        [When(@": I add new profile record")]
+        public void WhenIAddNewProfileRecord()
+        {
+            //throw new PendingStepException();
+        }
+
+        [Then(@":The profile record should be added succsfully")]
+        public void ThenTheProfileRecordShouldBeAddedSuccsfully()
+        {
+            //throw new PendingStepException();
+        }
+    }
+}
---
+++ MarsQA-1/Feature/Login.feature
@@ -0,0 +1,9 @@
+﻿Feature: As a seller I would like to add profile detaila so that people
+         seeking for some skills can look in to my details
+
+@mytag
+Scenario: Add new profile with valid details
+	Given : I logged into the URL succsfully
+	And : I navigate to profilepage
+	When : I add new profile record
+	Then :The profile record should be added succsfully
