f98d9b2f6a6d29308acae7dbb17dcccb91ec17fc
Sreena Narikot <sreesat25@yahoo.co.in>
2022-02-05 14:23:53 +0800

    added assertions for language

--- MarsQA-1/Feature/Login.feature
+++ MarsQA-1/Feature/Login.feature
@@ -1,9 +1,7 @@
 ﻿Feature: As a seller I would like to add profile detaila so that people
          seeking for some skills can look in to my details
 
-@mytag
-Scenario: Add new profile with valid details
-	Given : I logged into the URL succsfully
-	And : I navigate to profilepage
-	When : I add new profile record
-	Then :The profile record should be added succsfully
+Scenario:: ProfileLanguage creation
+	Given I am on my Profile Page
+	When I Click Addnew Language with valid details
+	Then Profile page is created successfully
---
+++ MarsQA-1/Stepdefinitions/Login.cs
@@ -0,0 +1,49 @@
+﻿using MarsQA_1.Helpers;
+using MarsQA_1.Pages;
+using MarsQA_1.SpecflowPages.Pages;
+using MarsQA_1.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature 
+{
+    [Binding]
+    class Login : Driver
+    {
+        ProfileRecord profileRecord = new ProfileRecord();
+        HomePage homePage = new HomePage();
+
+        [Given(@"I am on my Profile Page")]
+        public void GivenIAmOnMyProfilePage()
+        {
+           
+                HomePage homePage = new HomePage();
+                homePage.GoToProfile(driver);
+        }
+
+        [When(@"I Click Addnew Language with valid details")]
+        public void WhenIClickAddnewLanguageWithValidDetails()
+        {
+            ProfileRecord profileRecord = new ProfileRecord();
+            profileRecord.Createlanguagerecord(driver);
+        }
+
+        [Then(@"Profile page is created successfully")]
+        public void ThenProfilePageIsCreatedSuccessfully()
+        {
+            ProfileRecord profileRecord = new ProfileRecord();
+            string language = profileRecord.Getlanguage(driver);
+            Assert.That(language == "English", "Actual language and Expected do not match");
+            string level = profileRecord.Getlevel(driver);
+            Assert.That(level == "Conversational", "Actual level and Expected level do not match");
+   
+        }
+
+    }
+}
