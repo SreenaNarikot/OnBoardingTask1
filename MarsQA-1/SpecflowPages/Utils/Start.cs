c4a6516631fc0926bd460705ee792acf3bce4084
Sreena Narikot <sreesat25@yahoo.co.in>
2022-02-07 09:17:52 +0800

    login credentials changed

--- MarsQA-1/SpecflowPages/Utils/Start.cs
+++ MarsQA-1/SpecflowPages/Utils/Start.cs
@@ -20,9 +20,9 @@ namespace MarsQA_1.Utils
         {
             //launch the browser
             Initialize();
-            ExcelLibHelper.PopulateInCollection(@"MarsQA-1\SpecflowTests\Data\Mars.xlsx", "Credentials");
+            //ExcelLibHelper.PopulateInCollection(@"MarsQA-1\SpecflowTests\Data\Mars.xlsx", "Credentials");
             //call the SignIn class
-            SignIn.SigninStep();
+            SignIn.Login();
         }
 
         [AfterScenario]
